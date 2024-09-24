8b7a52240ba09a1401356e84b36b59206393ba41
briallman <brianaloftus11@gmail.com>
2024-09-24 17:14:56 -0400

    help me please

--- Function.R
+++ Function.R
@@ -1,5 +1,6 @@
 #Learing how to make a function
 
+#solivng a Problem
 foo <- function(x,y) {
   z <- x + y
   
@@ -10,3 +11,10 @@ foo (12,34)
 foo (2,20)
 foo (23,55)
 foo (12,55)
+
+#Spitting out 100 normal variables
+bar <- function() {
+  return(rnorm(100))
+}
+
+bar()
